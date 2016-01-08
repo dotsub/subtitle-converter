52f358d4a8b309885ccc4a9e5a6f048ea4a72dff
Brooks Lyrette <brooks@dotsub.com>
2016-01-08 08:13:44 -0500

    Added checkstyle to build. The defauls spring-boot project used tabs (those monsters) replaced with spaces

--- src/main/java/com/dotsub/converter/SubtitleConverterApplication.java
+++ src/main/java/com/dotsub/converter/SubtitleConverterApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SubtitleConverterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SubtitleConverterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SubtitleConverterApplication.class, args);
+    }
 }
