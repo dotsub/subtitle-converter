f89c6e6e188f4a0e8a8d07deca83094248023f26
Brooks Lyrette <brooks@dotsub.com>
2016-01-08 08:08:06 -0500

    Inital spring boot gradle project

---
+++ src/main/java/com/dotsub/converter/SubtitleConverterApplication.java
@@ -0,0 +1,12 @@
+package com.dotsub.converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubtitleConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubtitleConverterApplication.class, args);
+	}
+}
